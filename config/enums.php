63848837dfdbb7796805b25d4f16646c7a16d151
ryanpastoriza <ryanpastoriza@gmail.com>
2022-03-17 12:51:32 +0800

    added Configs

--- config/enums.php
+++ config/enums.php
@@ -15,5 +15,43 @@ return [
 		'Divorced',
 		'Separated',
 	],
+
+	'mode' => [
+		'Monthly',
+		'Bi-Monthly',
+		'Weekly',
+		'Lumpsum',
+	],
+
+	'type' => [
+		'Add-On',
+		'Prepaid',
+	],
+
+	'id_type' => [
+		'GSIS  UMID',
+		'SSS',
+		'Postal ID',
+		'Passport',
+		'PNP',
+		'4P’s ID',
+		'Brgy ID',
+		'Company ID',
+		'Pag-Ibig',
+		'Philhealth',
+		'TIN',
+		'NBI Clearance',
+		'Voter’s ID / Certificate',
+		'Police Clearance',
+		'Drivers Licence',
+		'Solo Parent ID',
+		'PWD ID',
+	],
+
+	'release_type' => [
+		'Cash Release',
+		'Check Release',
+		'Restructure Release',
+	],
 	
 ];
