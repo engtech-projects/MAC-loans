f30ee76b9cef7a93a5776a9d2c3e0beb9abf7fa8
prosbite <brainsnap.2012@gmail.com>
2022-06-27 14:51:43 +0800

    fixed release edit

--- resources/views/transaction/rejected_release_edit.blade.php
+++ resources/views/transaction/rejected_release_edit.blade.php
@@ -3,8 +3,7 @@
 @section('content')
     <!-- Main content -->
     <section class="content mb-45 app">
-		<!-- <rejected-release-edit id="{{request()->id}}" title="Rejected Account - Edit Information"  token="{{Session::get('token')}}"></rejected-release-edit> -->
-		<release-entry rejectid="{{request()->id}}" idtype="{{json_encode(config('enums.id_type'))}}" token="{{Session::get('token')}}" title="Rejected Account - Edit Information"></release-entry>
+		<rejected-release-edit id="{{request()->id}}" title="Rejected Account - Edit Information" idtype="{{json_encode(config('enums.id_type'))}}"  token="{{Session::get('token')}}"></rejected-release-edit>
     </section>
     <!-- /.content -->
 	<!-- MODALS  -->
