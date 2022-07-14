f53b8d6230dd9dab4be27f09e34317b9cff770e2
ryanpastoriza <ryanpastoriza@gmail.com>
2022-07-14 12:44:16 +0800

    cash voucher

--- app/Http/Resources/LoanAccount.php
+++ app/Http/Resources/LoanAccount.php
@@ -15,7 +15,7 @@ class LoanAccount extends JsonResource
     public function toArray($request)
     {
         return [
-        'loan_account_id' => $this->loan_account_id,
+            'loan_account_id' => $this->loan_account_id,
             'account_num' => $this->account_num,
             'date_release' => $this->date_release,
             'cycle_no' => $this->cycle_no,
@@ -64,6 +64,7 @@ class LoanAccount extends JsonResource
             'current_amortization' => $this->getCurrentAmortization(),
             'last_transaction' => null,
             'document' => $this->documents,
+            'cash_voucher' => $this->cashVoucher(),
             'payments' => $this->payments,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             TestDataSeeder::class,
             AccessibilitySeeder::class,
+            GLSeeder::class,
         ]);
     }
 }
