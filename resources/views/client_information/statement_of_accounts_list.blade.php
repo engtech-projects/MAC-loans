8031fa66a80607970cc3fb06a3669dd3dd15506b
EngtechPC1 <janlofredy@gmail.com>
2022-09-26 11:03:35 +0800

    Update statement_of_accounts_list.blade.php

--- resources/views/client_information/statement_of_accounts_list.blade.php
+++ resources/views/client_information/statement_of_accounts_list.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <!-- Main content -->
     <section class="content mb-45 app" id="app">
-		<client-list token="{{Session::get('token')}}" title="Statement of Account List" url="{{route('client_information.account_statement_details', ['id'=>':id'])}}"></client-list>
+		<client-list pbranch="{{Session::get('branch')->branch_id}}" token="{{Session::get('token')}}" title="Statement of Account List" url="{{route('client_information.account_statement_details', ['id'=>':id'])}}"></client-list>
     </section>
     <!-- /.content -->
 	<!-- MODALS  -->
