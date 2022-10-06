71c6fbce352c4a5554c2d6abf9ffe63e5e252f3e
EngtechPC1 <janlofredy@gmail.com>
2022-10-06 13:44:22 +0800

    total due fix rebates

--- resources/js/ui/transaction/repayment_entry/RepaymentDetails.vue
+++ resources/js/ui/transaction/repayment_entry/RepaymentDetails.vue
@@ -937,7 +937,7 @@ export default {
 			return this.totalPrincipal > this.loanAccount.current_amortization.advance_principal ? this.totalPrincipal - this.loanAccount.current_amortization.advance_principal : 0;
 		},
 		totalDue:function(){
-			return this.duePrincipal + this.dueInterest + this.duePdi + this.duePenalty;
+			return this.duePrincipal + this.dueInterestRebates + this.duePdi + this.duePenalty;
 		},
 		excessAdvancePrincipal:function(){
 			return this.loanAccount.current_amortization.advance_principal < this.totalPrincipal ? 0 : this.loanAccount.current_amortization.advance_principal - this.totalPrincipal;
