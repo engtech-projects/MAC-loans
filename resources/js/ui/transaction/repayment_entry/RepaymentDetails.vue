e7c6cf4a303ae406316cd005b3a8abf9ba6d70dc
EngtechPC1 <janlofredy@gmail.com>
2022-11-04 15:04:13 +0800

    fix last transaction date repayment entry

--- resources/js/ui/transaction/repayment_entry/RepaymentDetails.vue
+++ resources/js/ui/transaction/repayment_entry/RepaymentDetails.vue
@@ -1034,7 +1034,7 @@ export default {
 			return this.pborrower.photo? this.pborrower.photo : this.baseURL()+'/img/user.png';
 		},
 		lastTransactionDate:function(){
-			return this.loanAccount.current_amortization.lastPayment? this.dateToMDY2(new Date()) : 'None';
+			return this.loanAccount.payments.length > 0 ? this.dateToMDY2(new Date(this.loanAccount.payments[this.loanAccount.payments.length-1].updated_at)) : 'None';
 		},
 		amountDistributed:function(){
 			return parseFloat(this.payment.amount_paid) + parseFloat(this.loanAccount.current_amortization.advance_principal)
