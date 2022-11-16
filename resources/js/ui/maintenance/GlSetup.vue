9e1da7e270b2dd6855831429f16ebbc2d7c38b8f
EngtechPC1 <janlofredy@gmail.com>
2022-11-16 17:11:24 +0800

    revert gl setup

--- resources/js/ui/maintenance/GlSetup.vue
+++ resources/js/ui/maintenance/GlSetup.vue
@@ -1,106 +1,75 @@
 <template>
-	<div>
+	<div class="container-fluid" style="padding:0!important">
+		<notifications group="foo" />
 		<div class="mb-16"></div>
 		<div class="ml-16 mb-24 bb-primary-dark pb-7 text-block">
 			<h1 class="m-0 font-35">General Ledger Setup</h1>
 		</div><!-- /.col -->
-		<div class="d-flex ml-16">
-			<div class="flex-2">
-				<span class="text-20 py-7 mid-light-bb text-block text-primary-dark text-bold mb-12">Access</span>
-				<div class="p-12 light-border">
-					<table class="table table-stripped th-blue th-nbt">
-						<thead>
-							<th>Loan's Portal</th>
-							<th>Code</th>
-							<th>Accounting Portal</th>
-							<th>Account</th>
-						</thead>
-						<tbody>
-							<tr>
-								<td>Loan Receivable</td>
-								<td>1205</td>
-								<td>Loan's Receivable - Current</td>
-								<td>Releasing Account</td>
-							</tr>
-							<tr>
-								<td>Loan Receivable</td>
-								<td>1205</td>
-								<td>Loan's Receivable - Current</td>
-								<td>Releasing Account</td>
-							</tr>
-							<tr>
-								<td>Loan Receivable</td>
-								<td>1205</td>
-								<td>Loan's Receivable - Current</td>
-								<td>Releasing Account</td>
-							</tr>
-							<tr>
-								<td>Loan Receivable</td>
-								<td>1205</td>
-								<td>Loan's Receivable - Current</td>
-								<td>Releasing Account</td>
-							</tr>
-							<tr>
-								<td>Loan Receivable</td>
-								<td>1205</td>
-								<td>Loan's Receivable - Current</td>
-								<td>Releasing Account</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				
+		<div class="d-flex flex-column flex-xl-row ml-16">
+			<div style="flex:20">
+				<section class="mb-24" style="flex:21;padding-left:16px;">
+					<span class="section-title section-subtitle mb-12">List</span>
+					<div class="p-16 light-border">
+						<table class="table table-stripped th-nbt table-hover">
+							<thead>
+								<th>Loan's Portal</th>
+								<th>Code</th>
+								<th>Accounting Portal</th>
+								<th>Account</th>
+								<th></th>
+							</thead>
+							<tbody>
+								<tr v-if="!ledgerAccounts.length">
+									<td>No products found.</td>
+									<td></td>
+									<td></td>
+									<td></td>
+									<td></td>
+								</tr>
+								<tr v-for="la in ledgerAccounts" :key="la.gl_id">
+									<td>{{sentenceCase(la.loans)}}</td>
+									<td>{{la.code}}</td>
+									<td>{{sentenceCase(la.accounting)}}</td>
+									<td>{{sentenceCase(la.type)}}</td>
+									<td></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</section>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-export default {
-    props: ["token"],
-    data() {
-        return {
-            csrf: document
-                .querySelector('meta[name="csrf-token"]')
-                .getAttribute("content"),
-            ledgerAccounts: [],
-            newAccount: "",
-            accounts: [],
-            accountErr: "",
-            inputs: {
-                loans: "",
-                code: "",
-                accounting: "",
-                type: "",
-                gl_id: null,
-            },
-        };
-    },
-    methods: {
-        fetchLedgerAccounts: function () {
-            axios
-                .get(this.baseURL() + "api/gl", {
-                    headers: {
-                        Authorization: "Bearer " + this.token,
-                        "Content-Type": "application/json",
-                        Accept: "application/json",
-                    },
-                })
-                .then(
-                    function (response) {
-                        this.ledgerAccounts = response.data.data;
-                        this.accounts = this.setAccounts();
-                    }.bind(this)
-                )
-                .catch(
-                    function (error) {
-                        console.log(error);
-                    }.bind(this)
-                );
-        },
-    },
-    mounted() {
-        this.fetchLedgerAccounts();
-    },
-};
+    export default {
+		props:['token'],
+		data(){
+			return {
+				csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+				ledgerAccounts:[],
+			}
+		},
+		methods: {
+			fetchLedgerAccounts: function(){
+				axios.get(this.baseURL() + 'api/gl', {
+					headers: {
+						'Authorization': 'Bearer ' + this.token,
+						'Content-Type': 'application/json',
+						'Accept': 'application/json'
+					}
+				})
+				.then(function (response) {
+					this.ledgerAccounts = response.data.data;
+				}.bind(this))
+				.catch(function (error) {
+					console.log(error);
+				}.bind(this));
+			},
+		},
+        mounted() {
+           	this.fetchLedgerAccounts();
+        }
+    }
 </script>
