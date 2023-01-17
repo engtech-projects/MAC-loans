c7b196965ed0188bb2f2aeffded88c0fd0b9e090
josembn <35553424+janlofredy@users.noreply.github.com>
2023-01-17 15:03:35 +0800

    reverse addon prepaid

--- app/Models/Amortization.php
+++ app/Models/Amortization.php
@@ -91,10 +91,10 @@ class Amortization extends Model
                 'loan_account_id' => $account->loan_account_id,
                 'amortization_date' => $amortizationDate->toDateString(),
                 'principal' => number_format($principal, 2),
-                'interest' => number_format(strtolower($account->type) == "prepaid" ? $interest : 0, 2),
-                'total' => number_format(strtolower($account->type) == "prepaid" ? $total : $principal, 2),
+                'interest' => number_format(strtolower($account->type) != "prepaid" ? $interest : 0, 2),
+                'total' => number_format(strtolower($account->type) != "prepaid" ? $total : $principal, 2),
                 'principal_balance' => number_format($principalBalance, 2),
-                'interest_balance' => number_format(strtolower($account->type) == "prepaid" ? $interestBalance : 0, 2),
+                'interest_balance' => number_format(strtolower($account->type) != "prepaid" ? $interestBalance : 0, 2),
                 'status' => 'open',
             ];
 
