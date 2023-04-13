2a0cc3a052cf397090746258ecba93a61c9dfdfb
John Kenneth Suse <susejohnkenneth@gmail.com>
2023-04-13 14:06:33 +0800

    fix favicon

--- resources/views/_layout/app.blade.php
+++ resources/views/_layout/app.blade.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="icon" href="{{ url('favicon.ico') }}">
   <title> {{isset($title)? $title : 'Title'}} </title>
   
   @include('_includes.styles')
