512f81a0d8e04e69224a56a8ad54b8181c087c2c
prosbite <brainsnap.2012@gmail.com>
2023-05-12 10:23:37 +0800

    fixed toUpperCase error

--- resources/js/ui/transaction/override_release/OverrideReleaseDetails.vue
+++ resources/js/ui/transaction/override_release/OverrideReleaseDetails.vue
@@ -528,7 +528,7 @@
 					<div class="d-flex flex-column" style="padding:0 50px;">
 						<div class="d-flex flex-row align-items-center mb-24 darker-bb pb-10">
 							<div class="flex-1">
-								<span class="text-primary-dark font-26">{{pbranch.branch_name.toUpperCase()}} BRANCH ({{pbranch.branch_code}})</span>
+								<span class="text-primary-dark font-26">{{pbranch.branch_name}} BRANCH ({{pbranch.branch_code}})</span>
 							</div>
 							<div class="d-flex flex-column">
 								<span class="font-26 text-bold text-primary-dark lh-1">CASH VOUCHER</span>
@@ -592,7 +592,7 @@
 								</div>
 							</div>
 							<p class="mb-45">
-								Received payment from MICRO ACCESS LOANS CORPORATION - {{pbranch.branch_name.toUpperCase()}} BRANCH ({{pbranch.branch_code}}) The sum of <span class="allcaps">{{numToWords(parseFloat(loanaccount.net_proceeds))}}</span> Pesos only.
+								Received payment from MICRO ACCESS LOANS CORPORATION - {{pbranch.branch_name}} BRANCH ({{pbranch.branch_code}}) The sum of <span class="allcaps">{{numToWords(parseFloat(loanaccount.net_proceeds))}}</span> Pesos only.
 							</p>
 							<div class="d-flex flex-row">
 								<span class="flex-1">Received By: <span class="text-lg text-bold" style="margin-left:10px;border-bottom:1px solid #222;padding-right:100px;">{{loanaccount.borrower.firstname + ' ' + loanaccount.borrower.middlename.charAt(0).toUpperCase() + '. ' + loanaccount.borrower.lastname}}</span></span>
