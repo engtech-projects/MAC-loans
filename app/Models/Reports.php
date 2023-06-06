5797d43f96515b42c1be3fed7c995a2d10c85bdf
Genesis John Abobo <geiizanityy@gmail.com>
2023-06-06 14:11:36 +0800

    Convert date format

--- app/Models/Reports.php
+++ app/Models/Reports.php
@@ -1122,8 +1122,8 @@ class Reports extends Model
                                 $accOfficers[$aoKey]["products"][$prodValue["product_name"]]["centers"][$centVal["center"]]['accounts'][] = [
                                     'borrower_name' => isset($account->borrower) ? $account->borrower->fullname() : '',
                                     "account_num" => $account->account_num,
-                                    "date_loan" => $account->date_release,
-                                    "maturity" => $account->due_date,
+                                    "date_loan" => Carbon::createFromFormat('Y-m-d',$account->date_release)->format('m/d/Y'),
+                                    "maturity" => Carbon::createFromFormat('Y-m-d',$account->due_date)->format('m/d/Y'),
                                     "amount_loan" => $account->loan_amount,
                                     "principal_amount" => abs(($amortPrincipal + $shortPrincipal) - $advPrincipal),
                                     "interest_amount" => abs($amortInterest + $shortInterest - $advInterest),
