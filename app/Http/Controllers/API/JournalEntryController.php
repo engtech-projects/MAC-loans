41ab0419f2f28633a9fc5c34e01aa75dd0176ea2
Genesis John Abobo <geiizanityy@gmail.com>
2023-07-07 11:47:24 +0800

    Save prepaid interest reports

--- app/Http/Controllers/API/JournalEntryController.php
+++ app/Http/Controllers/API/JournalEntryController.php
@@ -1,40 +1,30 @@
 <?php
 
 namespace App\Http\Controllers\API;
-
-use App\Models\JournalEntry;
-use App\Models\LoanAccount;
+use App\Models\{
+    JournalBook,
+    JournalEntry,
+    EndTransaction
+};
 use Illuminate\Http\Request;
-use App\Http\Resources\JournalEntry as JournalEntryResource;
-use App\Models\EndTransaction;
-use App\Models\JournalBook;
 
 class JournalEntryController extends BaseController
 {
-    protected $journalBook;
-    public function __construct(JournalBook $journalBook)
+    protected $journalEntry;
+    public function __construct(JournalEntry $journalEntry)
     {
-        $this->journalBook = $journalBook;
+        $this->journalEntry = $journalEntry;
+
     }
 
     public function store(Request $request)
     {
-        $journalEntry = new JournalEntry();
-        $eod = new EndTransaction();
-        $transactionDateNow = $eod->getTransactionDate($request->input()["branch_id"])->date_end;
-        $journalBook = $this->journalBook->getJounalBookById($request->input()["journal_id"]);
-        $created = $journalEntry::create([
-            'journal_no'            =>          $journalBook->book_code,
-            'book_id'               =>          $journalBook->book_id,
-            'journal_date'          =>          $transactionDateNow,
-            'branch_id'             =>          $request->branch_id,
-            'source'                =>          'Releases',
-            'amount'                =>          $request->amount,
-            'status'                =>          'unposted',
-            'remarks'               =>          'Remarks',
-        ]);
 
-        return $this->sendResponse($created, 'Reports saved.');
+        $journalEntry = $this->journalEntry->createJournalEntry($request);
+        if($journalEntry) {
+
+        }
+        return $this->sendResponse($journalEntry, 'Journal Entry Saved.');
 
     }
 }
