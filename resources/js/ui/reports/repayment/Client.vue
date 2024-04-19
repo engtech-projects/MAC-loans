48f23d679eaadf9de2d9008413fff038e99f8850
Rizaly Beniga <78832093+benigarizaly@users.noreply.github.com>
2024-04-19 16:47:36 +0800

    cont. to total VAT PDI and Interest
    cont. to total VAT PDI and Interest

--- resources/js/ui/reports/repayment/Client.vue
+++ resources/js/ui/reports/repayment/Client.vue
@@ -87,8 +87,8 @@
 								<td>{{formatToCurrency(r.overpayment)}}</td>
 								<td>{{formatToCurrency(r.rebates)}}</td>
 								<td>{{formatToCurrency(r.total)}}</td>
-								<td>{{formatToCurrency(r.net_interest / 1.12*.12)}}</td>
-								<td>{{formatToCurrency(r.net_pdi / 1.12*.12)}}</td>
+								<td>{{formatToCurrency(r.net_interest)}}</td>
+								<td>{{formatToCurrency(r.net_pdi)}}</td>
 								<td>{{formatToCurrency(r.vat)}}</td>
 								<td>{{r.payment_type}}</td>
 							</tr>
@@ -327,7 +327,7 @@ export default {
 			return this.accountOfficers.filter(ao=>ao.status=='active'&&ao.branch_id==this.branch.branch_id);
 		},
 		total:function(){
-			var result = ['TOTAL','','',0,0,0,0,0,0,0,0,0,0,0,'',''];
+			var result = ['TOTAL','','',0,0,0,0,0,0,0,0,0,0,''];
 			this.reports.forEach(r => {
 				result[3] += r.amortization_principal;
 				result[4] += r.principal;
@@ -338,7 +338,7 @@ export default {
 				result[9] += r.rebates;
 				result[10] += r.total;
 				result[11] += r.net_interest;
-				result[12] += r.net_pdi;
+				result[12] += r.net_pdi / (1.12) *.12;
 				result[13] += r.vat;
 			});
 			return result;
