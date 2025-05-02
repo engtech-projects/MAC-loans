e21c3ea0c65351874d421511ce99e67ad13efbd5
John Kenneth Suse <susejohnkenneth@gmail.com>
2025-05-02 12:59:45 +0800

    Removed obsolete functions
    - Removed the implementation of min and max date restrictions on the date input, as AOP reports are now dynamic.
    - Generate button now directly triggers the report generation function.
    - Removed obsolete functions such as fetching dates from the endtransaction table, retrieving previously saved reports from the database, and other unused or commented-out code

--- resources/js/ui/reports/branch/AccountOfficer.vue
+++ resources/js/ui/reports/branch/AccountOfficer.vue
@@ -11,7 +11,7 @@
 			<span class="font-lg text-primary-dark" style="flex:3">Report</span>
 			<div class="d-flex flex-row align-items-center mr-24" style="flex:2">
 				<span class="mr-10">Date: </span>
-				<input v-model="filter.as_of" type="date" class="form-control" :min="dates.min_date" :max="dates.max_date">
+				<input v-model="filter.as_of" type="date" class="form-control">
 			</div>
 			<!-- <div class="d-flex flex-row align-items-center mr-64" style="flex:2">
 				<span class="mr-10">To: </span>
@@ -39,7 +39,7 @@
 				</select>
 			</div>
 			<div class="d-flex flex-row align-items-center mr-24 justify-content-start flex-1">
-				<button @click="generate()" class="btn btn-primary">Generate</button>
+				<button @click="fetchReports" class="btn btn-primary">Generate</button>
 			</div>
 			<!-- <div class="d-flex flex-row align-items-center" style="flex:3">
 				<span class="mr-10">AO: </span>
@@ -204,36 +204,6 @@ export default {
 		}
 	},
 	methods:{
-		generate:function(){
-			if(this.filter.as_of == this.transDate){
-				this.fetchReports();
-			}else{
-				this.fetchNoCurrentReports();
-			}
- 		},
-		async fetchDates(){
-			this.loading = true;
-			await axios.get(this.baseURL() + 'api/report/branch/performancereport/dates?branchId=' + this.selectedBranch.branch_code, {
-				headers: {
-					'Authorization': 'Bearer ' + this.token,
-					'Content-Type': 'application/json',
-					'Accept': 'application/json'
-				}
-			})
-			.then(function (response) {
-				this.loading = false;
-				const data = response.data.data;
-				this.dates.min_date = new Date(data.min_date).toISOString().slice(0, 10);
-				this.dates.max_date = new Date(data.max_date).toISOString().slice(0, 10);
-				// this.dates.max_date = this.transDate;
-				// console.log(response.data);
-				this.filter.as_of = this.dates.max_date;
-			}.bind(this))
-			.catch(function (error) {
-				this.loading = false;
-				console.log(error);
-			}.bind(this));
-		},
 		async fetchReports(){
 			this.loading = true;
 			await axios.post(this.baseURL() + 'api/report/branch', this.filter, {
@@ -245,28 +215,7 @@ export default {
 			})
 			.then(function (response) {
 				this.loading = false;
-				this.reports = response.data.data
-				// console.log(this.reports);
-			}.bind(this))
-			.catch(function (error) {
-				this.loading = false;
-				console.log(error);
-			}.bind(this));
-		},
-		async fetchNoCurrentReports(){
-			this.loading = true;
-			
-			await axios.post(this.baseURL() + 'api/report/branch/performancereport',{transaction_date:this.filter.as_of,
-				branch_id:this.selectedBranch.branch_code}, {
-				headers: {
-					'Authorization': 'Bearer ' + this.token,
-					'Content-Type': 'application/json',
-					'Accept': 'application/json'
-				}
-			})
-			.then(function (response) {
-				this.loading = false;
-				this.reports = response.data.data==='No reports found.'?[]:response.data.data;
+				this.reports = response.data.data;
 			}.bind(this))
 			.catch(function (error) {
 				this.loading = false;
@@ -419,21 +368,14 @@ export default {
 		}
 	},
 	watch:{
-		'filter.group'(val){
-			// if(val){
-			// 	this.fetchReports();
-			// }
-		},
 		selectedBranchId(newVal) {
 			this.filter.branch_id = newVal;
 			this.branch = this.branches.find(b => b.branch_id === newVal);
-			this.fetchDates();
 			this.reports = [];
 		}
 	},
 	mounted(){
 		this.branch = JSON.parse(this.pbranch);
-		this.filter.branch_id = this.branch.branch_id;
 		this.selectedBranchId = this.branch.branch_id;
 		this.fetchAo();
 		this.fetchTransactionDate();
