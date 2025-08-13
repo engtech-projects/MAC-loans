2d45945edc94a37c220c46dc300327c8b14b10ae
Rizaly Beniga <78832093+benigarizaly@users.noreply.github.com>
2025-08-13 15:08:45 +0800

    Reports / https - Repayment Report

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,13 +4,12 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
     public function register()
     {
@@ -19,11 +18,12 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        // ✅ Correct method name
+        URL::forceScheme('https');
     }
 }
